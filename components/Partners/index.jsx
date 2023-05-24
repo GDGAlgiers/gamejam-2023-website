00913cf164a1f1ba6b93176712dbb726f4fede00
LyCrash <kl_mouhoun@esi.dz>
2023-05-24 11:58:28 +0100

    Correcting the box and logo shape

--- components/Partners/index.jsx
+++ components/Partners/index.jsx
@@ -8,24 +8,20 @@ const Partners = () => {
 
         <div class="relative" >
           <img src={'/shapes/EllipseColor.png'} alt="EllipseColorMissed" 
-          class= " w-[231px] h-[231px] z-10 " />
-          <div class=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-            <img src={'/logos/MC.png'} alt="LogoMCMissed" 
-            class= "  w-[152.67px] h-[152.67px]  z-20 " />
+          class= " w-[231px] h-[231px]" />
+          <div class=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+          bg-contain bg-[url('/logos/MC.png')] w-[152.67px] h-[152.67px]">
           </div>
         </div>
 
-        <div class="relative bg-[url('/shapes/ColorTextFrame.png')] w-[278.58px] h-[93.13px] ">
-          <div class="absolute bg-[url('/shapes/TextFrame.png')] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[265.69px] h-[80.25px]">     
-            <p class="absolute text-gameJam-yellow text-4xl font-PTGilroy-Bold top-1/2 left-1/2 
-            transform -translate-x-1/2 -translate-y-1/2">Micro Club</p>
+        
+        <div class="relative w-[278.58px] h-[93.13px] bg-[url('/shapes/ColorTextFrame.png')] bg-contain">
+          <div class="flex items-center justify-center w-[265.69px] h-20 bg-[url('/shapes/TextFrame.png')] bg-cover
+                      absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <p class="text-gameJam-yellow text-4xl font-PTGilroy-Bold ">Micro Club</p>
           </div>
-        </div>
-
-
-
-      
-   
+        </div> 
+        
 
       </div>
     </section>
