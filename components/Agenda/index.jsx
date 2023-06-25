cf1aedfb18eecf388d802cb3c2159ef7d29499ca
Rayan-Allali <108684980+Rayan-Allali@users.noreply.github.com>
2023-06-25 14:01:13 +0100

    new agenda without content

--- components/Agenda/index.jsx
+++ components/Agenda/index.jsx
@@ -73,10 +73,36 @@ const Agenda = () => {
             "startTime":"00:30",
             "endTime":"01:00",
             "text": "To start, print out the hiking guide and map"
-        }]
+        }],
+        "04":[
+            {
+                "id":0,
+                "startTime":"00:00",
+                "endTime":"00:30",
+                "text": "To start, print out the hiking guide and map"
+            },
+            {
+                "id":1,
+                "startTime":"00:30",
+                "endTime":"01:00",
+                "text": "To start, print out the hiking guide and map"
+            }],
+            "05":[
+                {
+                    "id":0,
+                    "startTime":"00:00",
+                    "endTime":"00:30",
+                    "text": "To start, print out the hiking guide and map"
+                },
+                {
+                    "id":1,
+                    "startTime":"00:30",
+                    "endTime":"01:00",
+                    "text": "To start, print out the hiking guide and map"
+                }]
     }
     
-    const Days=["01","02","03"]
+    const Days=["01","02","03","04","05"]
     const updateDay = (day) => {
         setSelectedDay(day);
     };
@@ -84,42 +110,14 @@ const Agenda = () => {
     return (
         <section className="w-full h-full">
             <SectionTitle title="Agenda"/>
-            <div className="relative flex flex-col lg:flex-row lg:items-center lg:justify-center w-full min-h-screen">
-               <div className="flex  justify-center lg:block p-4 lg:p-0">
+            <div className="flex lg:flex-col items-center w-full min-h-screen">
+               <div className="flex flex-col lg:flex-row gap-8 justify-center p-4 lg:p-0">
                {Days.map((day) => {
                 return(
                         <Day key={day} dayString={day} position={selectedDay} updateSelectedDay={updateDay}/>
                 )
                }
                 )}
-               </div>
-               <div className="hidden lg:block 2xl:hidden">
-                <div className="absolute top-[28%] left-[25%]">
-                    <Image src={LeftShape.src} alt="Image description" width={100} height={180}></Image>
-                </div>
-                <div className="absolute bottom-[18%] right-[32%]">
-                    <Image src={BottomShape.src} alt="Image description" width={450} height={200}></Image>
-                </div>
-                <div className="absolute top-[15%] right-[25%]">
-                    <Image src={RightShape.src} alt="Image description" width={110} height={100}></Image>
-                </div>
-                <div className="absolute top-[2%] left-[28%]">
-                    <Image src={TopShape.src} alt="Image description" width={480} height={200}></Image>
-                </div>
-               </div>
-               <div className="hidden 2xl:block">
-                <div className="absolute top-[29%] left-[28%]">
-                    <Image src={LeftShape.src} alt="Image description" width={150} height={300}></Image>
-                </div>
-                <div className="absolute bottom-[12%] right-[34%]">
-                    <Image src={BottomShape.src} alt="Image description" width={600} height={200}></Image>
-                </div>
-                <div className="absolute top-[22%] right-[27%]">
-                    <Image src={RightShape.src} alt="Image description" width={130} height={110}></Image>
-                </div>
-                <div className="absolute top-[2%] left-[32%]">
-                    <Image src={TopShape.src} alt="Image description" width={600} height={200}></Image>
-                </div>
                </div>
                 <DayAgenda agenda={StupidAgenda[selectedDay]}></DayAgenda>
             </div>
