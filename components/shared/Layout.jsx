55311e0bf40a569458c8b1f48c088433daf223eb
Zouambia Sohaib <js_zouambia@esi.dz>
2023-06-26 18:53:13 +0100

    Added Register Route

--- components/shared/Layout.jsx
+++ components/shared/Layout.jsx
@@ -60,7 +60,7 @@ const Layout = ({ mainRef, sectionsRef, children }) => {
       <Shapes />
 
       {/* Background image */}
-      <div className="fixed   top-0 left-0 bottom-0 right-0 -z-1 bg-[url('/images/bg.png')] bg-cover bg-no-repeat bg-center"></div>
+      <div className="fixed top-0 left-0 bottom-0 right-0 -z-1 bg-[url('/images/bg.png')] bg-cover bg-no-repeat bg-center"></div>
 
       <div className="relative h-full">
         <Navbar />
