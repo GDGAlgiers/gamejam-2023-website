a6b5facef37908ff8ba59f68ff582c592b09c176
Ayyoub Kasmi <ayyoubkasmidz@gmail.com>
2023-06-26 13:18:40 +0200

    Comment out sponsors + refactor mobile navbar

--- data/nav.js
+++ data/nav.js
@@ -1,35 +1,47 @@
 const navItems = [
     {
         name: "Start",
+        target: "/#",
         isActive: true,
     },
     {
         name: "About",
+        target: "/#about",
         isActive: false
     },
     // {
     //     name: "Mentors",
-    //     isActive: false
+   
+//    target: "/#"
+ //     isActive: false
     // },
     {
         name: "Agenda",
+        target: "/#agenda",
         isActive: false
     },
     {
         name: "Partners",
+        target: "/#partners",
         isActive: false
     },
-    {
-        name: "Sponsors",
-        isActive: false
-    },
+    // {
+    //     name: "Sponsors",
+   
+    //  target: "/#sponsors" 
+    //     isActive: false
+    // },
     {
         name: "FaQ",
+        target: "/#faq",
         isActive: false
     },
     {
         name: "Contact",
+        target: "/#contact",
         isActive: false
     }
 ]
+
+
 export default navItems
